265555d11ab143751c228f3b2b70bf203277b632
Volodymyr Pivoshenko <volodymyr.pivoshenko@gmail.com>
2024-03-29 23:01:47 +0000

    feat: update components

--- src/common/component.js
+++ src/common/component.js
@@ -10,8 +10,7 @@ class Component extends HTMLElement {
       raleway: '<link href="https://fonts.googleapis.com/css?family=Raleway:600" rel="stylesheet">',
     },
     icons: {
-      material:
-        '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">',
+      material: '<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">',
       cryptofont: '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/monzanifabio/cryptofont/cryptofont.css">',
       tabler: '<link rel="stylesheet" href="src/css/tabler-icons.min.css">',
     },
@@ -23,18 +22,12 @@ class Component extends HTMLElement {
   constructor() {
     super();
 
-    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow = this.attachShadow({ mode: 'open' });
   }
 
-  style() {
-    return null;
-  }
-  template() {
-    return null;
-  }
-  imports() {
-    return [];
-  }
+  style()    { return null; }
+  template() { return null; }
+  imports()  { return []; }
 
   /**
    * Reference an external css file
@@ -53,7 +46,8 @@ class Component extends HTMLElement {
   get getResources() {
     const imports = this.imports();
 
-    if (this.resources?.style) imports.push(this.resources.style);
+    if (this.resources?.style)
+      imports.push(this.resources.style);
 
     return imports;
   }
@@ -65,7 +59,8 @@ class Component extends HTMLElement {
   async loadStyles() {
     let html = this.getResources.join("\n");
 
-    if (this.style()) html += `<style>${this.style()}</style>`;
+    if (this.style())
+      html += `<style>${this.style()}</style>`;
 
     return html;
   }
@@ -75,7 +70,8 @@ class Component extends HTMLElement {
    * @returns {string} html
    */
   async buildHTML() {
-    return (await this.loadStyles()) + (await this.template());
+    return await this.loadStyles() +
+          await this.template();
   }
 
   /**
@@ -99,7 +95,7 @@ class Component extends HTMLElement {
       set: (target, prop, value) => {
         this.shadow.querySelector(target[prop]).innerHTML = value;
         return true;
-      },
+      }
     });
   }
 
